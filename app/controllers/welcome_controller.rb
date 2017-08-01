16773285a69bc51d6b913a0e3bddb2ad1d654733
ccabo1 <ccabo@seas.upenn.edu>
2017-08-01 09:30:16 -0700

    form and alerd alert styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    @articles = Article.all
+    articles = Article.all.order(created_at: :desc)
+    @feature = articles.first
+    articles.delete(@feature)
+    @three = articles.limit(3)
+    articles.delete(@three)
+    @articles = articles.limit(4)
   end
 end
