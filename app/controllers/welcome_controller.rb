dfeccb36d59d2a022ad30d919dac171b624e6a96
ccabo1 <ccabo@seas.upenn.edu>
2017-08-06 11:33:33 -0700

    Query adjustment

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
   def index
     articles = Article.all.order(created_at: :desc)
     @feature = articles.first
-    @three = articles[1..4]
-    @articles = articles[4..8]
+    @three = articles.all[1..4]
+    @articles = articles.all[4..8]
   end
 
   def notfound
