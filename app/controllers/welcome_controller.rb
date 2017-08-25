80b73982165d96901a38743665d000a5a5dd73ff
ccabo1 <ccabo@seas.upenn.edu>
2017-08-25 18:01:02 +0200

    Better organization of home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
   def index
     articles = Article.where(published: true).order(created_at: :desc)
     @feature = articles.limit(1)[0]
-    @three = articles.offset(1).limit(3)
-    @eight = articles.offset(4).limit(8)
+    @three = articles.offset(1).where(region: 'undefined').limit(4)
+    @eight = articles.offset(1).where.not(region: 'undefined').limit(8)
   end
 
   def notfound
