d1db38771e08a1c2f6085b16c0d20df351554019
ccabo1 <ccabo@seas.upenn.edu>
2017-09-29 18:51:12 -0400

    navbar scroll updates

--- app/assets/javascripts/general/navbar-scroll.js
+++ app/assets/javascripts/general/navbar-scroll.js
@@ -1,23 +1,28 @@
 var last = 0;
+addNavListener();
 
 $(document).ready(function() {
   last = 0;
+  addNavListener();
 });
 
-window.addEventListener('scroll', function() {
-  var current = document.body.scrollTop;
-  if (current <= 20) {
-    $('.navbar').addClass('top');
-  } else {
-    $('.navbar').removeClass('top');
-  }
+function addNavListener() {
+  window.addEventListener('scroll', function() {
+    var current = document.documentElement.scrollTop;
 
-  if (Math.abs(current - last) >= 40) {
-    if (current >= 400 && current > last) {
-      $('.fixed.front').slideUp(200);
+    if (current <= 20) {
+      $('.navbar').addClass('top');
     } else {
-      $('.fixed.front').slideDown(200);
+      $('.navbar').removeClass('top');
     }
-    last = current;
-  }
-});
+
+    if (Math.abs(current - last) >= 40) {
+      if (current >= 400 && current > last) {
+        $('.fixed.front').slideUp(200);
+      } else {
+        $('.fixed.front').slideDown(200);
+      }
+      last = current;
+    }
+  });
+}
