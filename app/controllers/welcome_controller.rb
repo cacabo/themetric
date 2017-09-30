30bf793ea44069c87cf8cd0b2beace4326688162
ccabo1 <ccabo@seas.upenn.edu>
2017-09-30 18:37:35 -0400

    query updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,23 +1,23 @@
 class WelcomeController < ApplicationController
   def index
     # only show published articles
-    published = Article.where(published: true).where(featured: false)
+    @published = Article.where(published: true).where(featured: false)
 
     # find the feature
     @feature = Article.where(featured: true)[0]
 
     # find most read articles
-    by_views = published.order(views: :desc)
-    @popular = by_views.limit(4);
+    @by_views = @published.order(views: :desc)
+    @popular = @by_views.limit(4);
 
     # sort the remaining articles by time
-    by_time = by_views.offset(4).order(updated_at: :desc)
+    @by_time = @by_views.offset(4).order(updated_at: :desc)
 
     # find the articles
-    @articles = by_time.limit(24)
+    @articles = @by_time.limit(24)
 
     # suggest remaining articles
-    @more_popular = by_time.offset(24).limit(6);
+    @more_popular = @by_time.offset(24).limit(6);
   end
 
   def about
