ab5019e163183218fcc90449b4069ff2a05a233d
ccabo1 <ccabo@seas.upenn.edu>
2017-10-07 10:45:08 -0400

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :role, :location, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :role, :location, :facebook, :linkedin, :twitter, :instragram, :website, :github, :image])
   end
 end
