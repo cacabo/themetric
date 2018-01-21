a983c5601468419bc32e6787c5dcc31ba77c6d30
ccabo1 <ccabo@seas.upenn.edu>
2018-01-21 11:30:34 -0500

    Updating everything to published at

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,24 +1,20 @@
 class WelcomeController < ApplicationController
   def index
-    # find the feature
-    @feature = Article.where(featured: true)[0]
+    # Get 1 featured article
+    @feature = Article.find_by(featured: true)
 
-    # only show published articles
-    published = Article.where(published: true)
-    published = published.where(featured: false).or(published.where(featured: nil))
+    # Get all non-featured, published articles
+    non_featured = Article.where(published: true, featured: false)
+    @popular = non_featured.order(views: :desc).limit(3)
 
-    # find most read articles
-    by_views = published.order(views: :desc)
-    @popular = by_views.limit(3);
+    # Get the remaining articles
+    less_popular = non_featured.where.not(id: @popular)
 
-    # sort the remaining articles by time
-    by_time = by_views.offset(3).order(created_at: :desc)
-
-    # find the articles
-    @articles = by_time.limit(24)
-
-    # suggest remaining articles
-    @more_popular = by_time.offset(24).limit(6);
+    # Order the articles with published_at by published_at
+    # Append the articles without published_at ordered by created_at
+    remaining = less_popular.where.not(published_at: nil).order(published_at: :desc)
+    @articles = remaining.limit(24)
+    @more_popular = remaining.offset(24).limit(6)
   end
 
   def about
